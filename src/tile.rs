49b3bb18e1bff89a405470fee57e692d56287ce3
Earthgames <earthgame_s@outlook.com>
2024-06-15 11:51:33 +0200

    Tile?

--- src/tile.rs
+++ src/tile.rs
@@ -1,10 +1,34 @@
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Resource, Default, Serialize, Deserialize)]
+#[derive(Resource, Default, Serialize, Deserialize, Component)]
 pub enum Tile {
     #[default]
     Ground,
     Grass,
     Spike,
+}
+
+impl Tile {
+    fn make_tile(
+        tile: Tile,
+        position: Vec3,
+        mut commands: Commands,
+        asset_server: Res<AssetServer>,
+    ) {
+        let texture: Handle<Image> = asset_server.load(match tile {
+            _ => "test.png",
+        });
+        commands.spawn((
+            SpriteBundle {
+                texture,
+                transform: Transform {
+                    translation: position,
+                    ..default()
+                },
+                ..default()
+            },
+            tile,
+        ));
+    }
 }
